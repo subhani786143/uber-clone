1a4bf47311351c1fa878b23290673d1077c8941d
subhani786143 <167842890+subhani786143@users.noreply.github.com>
2024-09-30 17:46:20 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ubersubbu" # Replace with your actual S3 bucket name
+    bucket = "ubersubhani" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
